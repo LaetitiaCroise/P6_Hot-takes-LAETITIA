3ddfeb4f62fcdf252d558a364b2056b2c13fa4c6
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-11-25 14:04:12 +0100

    init projet

---
+++ back/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+ 
+module.exports = (req, res, next) => {
+   try {
+       const token = req.headers.authorization.split(' ')[1]; // fonction split pour récupéré aprrès l'espace tout le header , les erreurs s'aficherons dans le bloc catch
+       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // fonction veritfy pour décoder le token pour verifier sa validité
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+    //    si tout fonctione comme prévue on passe a la fonction next
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
