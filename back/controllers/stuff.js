cc40328c3f1107d6520f6e3253995e3ae1a6386f
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-11-25 15:00:20 +0100

    Create sauce ok

--- back/controllers/stuff.js
+++ back/controllers/stuff.js
@@ -1,21 +1,16 @@
-const Thing = require('../models/thing');
+const Sauce = require('../models/sauce');
 const fs = require('fs');
 
-exports.createThing = (req, res, next) => {
-    const thingObject = JSON.parse(req.body.thing);
-    delete thingObject._id;
-    delete thingObject._userId;
-    const thing = new Thing({
-        ...thingObject,
-        userId: req.auth.userId,
-        // likes: 0,
-        // dislikes: 0,
-        // usersDisliked: [],
-        // usersLiked: [],
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+
+    const sauce = new Sauce({
+        ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
   
-    thing.save()
+    sauce.save()
     .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
     .catch(error => { res.status(400).json( { error })})
 };
--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -6,10 +6,11 @@ const multer = require('../middleware/multer-config');
 const stuffCtrl = require('../controllers/stuff');
 
 router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
+router.post('/', auth, multer, stuffCtrl.createSauce);
 router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
+// router.post('/:id/like',auth, stuffCtrl.likeDislike);
 
 module.exports = router;
 
