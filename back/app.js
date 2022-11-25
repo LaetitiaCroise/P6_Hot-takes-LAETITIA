ea66c58c1cf6ed254daff3aa7ce6623a53ec4c14
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-11-25 14:22:07 +0100

    app.js ok

--- back/app.js
+++ back/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const stuffRoutes = require('./routes/stuff');
+const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 const path = require('path');
 
@@ -22,7 +22,7 @@ mongoose.connect('mongodb+srv://Mufasha:Lucas2203!@cluster0.h0omq8l.mongodb.net/
   });
 
 app.use(bodyParser.json());
-app.use('/api/stuff', stuffRoutes);
+app.use('/api/sauces', saucesRoutes);
 app.use ('/api/auth',userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 module.exports = app;
